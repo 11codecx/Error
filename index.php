1ced75af5bf59250368c08971bcf5475dc3b8727
11codecx <77472631+11codecx@users.noreply.github.com>
2021-01-14 21:24:29 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> 
+	<link rel="stylesheet" type="icon/png" href="https://www.eluniverso.com/sites/default/files/styles/powgallery_1024/public/fotos/2015/12/youtube-icon-full_color.png?itok=z6C7Hyhy">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<title>Codecx</title>
+</head>
+<body>
+
+	<nav id="Nav">
+		<h1>Unknown Error 511</h1>
+	</nav>
+
+</body>
+</html>
